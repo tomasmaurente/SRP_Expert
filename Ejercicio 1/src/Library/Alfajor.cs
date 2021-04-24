9832e74413fcf3f6dd89d00efff22d8c595c419a
IngMatias <matias.hernandez@correo.ucu.edu.uy>
2021-04-24 19:32:06 -0300

    Comentarios sobre expert y srp (PARTE1)

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -1,5 +1,14 @@
 using System;
 
+/*
+    La clase "Alfajor" cumple correctamente las indicaciones del principio Expert, 
+    asigna la responsabilidad del Constructor al que posee la informacion, las variables de instancia.
+    Si una crítica se puede hacer es que no aprovecha sus beneficios, no matiene encapsulada la clase.
+
+    El patron SRP se cumple; ya que unicamente tiene la responsabilidad de crear y asignar 
+    las variables de instancia.
+*/
+
 namespace Expert_SRP
 {
     public class Alfajor 
--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -1,5 +1,23 @@
 using System;
 
+/*
+
+    La clase "Kiosco" no cumple correctamente el patron SRP; 
+    ya que tiene la responsabilidad de determinar si se puede comprar un alfajor
+    y además tiene la responsabilidad de transformar dolares a pesos, en caso de ser necesario.
+
+    Por otro lado, si nos acercamos a la siguiente definicion del patron SRP
+    "Una clase debe tener solo una razón para cambiar",
+    podemos encontrar por ejemplo que si en algun momento cambia la formula de el calculo del precio 
+    "a.PrecioDulce + a.PrecioMasa"
+    y se toma en cuenta por ejemplo el precio del envoltorio, deberiamos recurrir al metodo "PuedeComprar"
+    y alterarlo. 
+    Ademas, en caso de que el precio de relacion entre el peso y el dolar se altere 
+    deberiamos alterar el metodo "ConvertirAPesos".
+    Por lo tanto, observamos dos razones de cambio para una sola clase lo que viola el patron SRP.
+
+*/
+
 namespace Expert_SRP
 {
     public class Kiosco 
