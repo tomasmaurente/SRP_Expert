8c32729ff81114eb498a671f4562f629753d30bf
IngMatias <matias.hernandez@correo.ucu.edu.uy>
2021-04-25 20:12:51 -0300

    Ejercicio2, parte 2

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -9,9 +9,14 @@ namespace SRP
         {
             Libro libro1 = new Libro("Design Patterns","Erich Gamma & Others","001-034");
             Libro libro2 = new Libro("Pro C#","Troelsen","001-035");
-            libro1.AlmacenarLibro("A","7");
-            libro2.AlmacenarLibro("B","3");
+            Biblioteca estante1 = new Biblioteca("A","7");
+            Biblioteca estante2 = new Biblioteca("B","3");
+            estante1.AlmacenarLibro(libro1);
+            estante2.AlmacenarLibro(libro2);
 
+            Console.WriteLine(libro1);
+            Console.WriteLine(libro1.Ubicacion);
+            Console.WriteLine(libro2.Ubicacion);
         }
     }
 }
