b36a850d38694d6aa3ebd0b1b7261064fa1d383c
IngMatias <matias.hernandez@correo.ucu.edu.uy>
2021-04-27 18:47:20 -0300

    Algunas correcciones

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -4,9 +4,7 @@ namespace Expert_SRP
 {
     public class Alfajor 
     {
-        private Double precioDulce;
         public Double PrecioDulce {get;set;}
-        private Double precioMasa;
         public Double PrecioMasa {get; set;}
         public Alfajor(double precioMasa, double precioDulce)
         {
